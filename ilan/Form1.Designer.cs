8fd21452704788ae1f3a9c64c6149c4de576637e
cavidan888 <cavidan666@gmail.com>
2018-04-05 12:55:15 +0400

    snake

---
+++ ilan/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ilan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Down = new System.Windows.Forms.Button();
+            this.uP = new System.Windows.Forms.Button();
+            this.Right = new System.Windows.Forms.Button();
+            this.Left = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Down
+            // 
+            this.Down.Location = new System.Drawing.Point(629, 395);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(59, 23);
+            this.Down.TabIndex = 1;
+            this.Down.Text = "Down";
+            this.Down.UseVisualStyleBackColor = true;
+            this.Down.Click += new System.EventHandler(this.Down_Click_1);
+            // 
+            // uP
+            // 
+            this.uP.Location = new System.Drawing.Point(630, 337);
+            this.uP.Name = "uP";
+            this.uP.Size = new System.Drawing.Size(59, 23);
+            this.uP.TabIndex = 2;
+            this.uP.Text = "Yuxari";
+            this.uP.UseVisualStyleBackColor = true;
+            this.uP.Click += new System.EventHandler(this.uP_Click_1);
+            // 
+            // Right
+            // 
+            this.Right.Location = new System.Drawing.Point(697, 366);
+            this.Right.Name = "Right";
+            this.Right.Size = new System.Drawing.Size(59, 23);
+            this.Right.TabIndex = 3;
+            this.Right.Text = "Sag";
+            this.Right.UseVisualStyleBackColor = true;
+            this.Right.Click += new System.EventHandler(this.Right_Click);
+            // 
+            // Left
+            // 
+            this.Left.Location = new System.Drawing.Point(561, 366);
+            this.Left.Name = "Left";
+            this.Left.Size = new System.Drawing.Size(59, 23);
+            this.Left.TabIndex = 4;
+            this.Left.Text = "Sol";
+            this.Left.UseVisualStyleBackColor = true;
+            this.Left.Click += new System.EventHandler(this.Left_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 425);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.uP);
+            this.Controls.Add(this.Right);
+            this.Controls.Add(this.Left);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Button uP;
+        private System.Windows.Forms.Button Right;
+        private System.Windows.Forms.Button Left;
+    }
+}
+
---
+++ ilan/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ilan
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> ilanParca = new List<PictureBox>();
+        PictureBox Snake;
+        PictureBox Meal;
+        int topPos;
+        int leftPos;
+        public Form1()
+        {
+            InitializeComponent();
+            createSnake();
+            CreateMeal();
+            
+        }
+
+
+        public void createSnake()
+        {
+           
+                Snake = new PictureBox();
+                Snake.Width = 10;
+                Snake.Height = 10;
+                Snake.BackColor = Color.Red;
+                Snake.Location = new Point(100, 100);
+                Snake.Left += 40;
+                Snake.Top = 150;
+                Controls.Add(Snake);
+                ilanParca.Add(Snake);
+            
+
+           
+
+        }
+
+        public void CreateMeal()
+        {
+            //Meal = new PictureBox();
+            //Meal.Width = 10;
+            //Meal.Height = 10;
+            //Meal.BackColor = Color.Red;
+            //Meal.Location = new Point(300, 150);
+            //Controls.Add(Meal);
+
+
+            Random rnd = new Random();
+            int MealLeft = rnd.Next(0, this.Width / 40);
+            int MealTop = rnd.Next(0, this.Height / 40);
+            MealTop = MealTop * 40;
+            MealLeft = MealLeft * 40;
+            if (MealLeft % 40 == 0 && MealTop % 40 == 0)
+            {
+                Meal = new PictureBox();
+                Meal.Width = 10;
+                Meal.Height = 10;
+                Meal.BackColor = Color.Green;
+                Meal.Left = MealLeft;
+                Meal.Top = MealTop;
+                Controls.Add(Meal);
+            }
+            else
+            {
+                CreateMeal();
+            }
+
+        }
+
+        public void YuxariGet()
+        {
+            if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
+            {
+               
+
+                Snake = new PictureBox();
+                Snake.Width = 10;
+                Snake.Height = 10;
+                Snake.BackColor = Color.Green;
+                Snake.Left = ilanParca[ilanParca.Count - 1].Left + 40;
+                Snake.Top = ilanParca[ilanParca.Count - 1].Top;
+                Controls.Add(Snake);
+                ilanParca.Add(Snake);
+                Meal.Hide();
+                CreateMeal();
+
+            }
+            topPos -= 10;
+            Snake.Top = topPos;
+        }
+
+        public void AsagiGet()
+        {
+
+            if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
+            {
+                
+
+                Snake = new PictureBox();
+                Snake.Width = 10;
+                Snake.Height = 10;
+                Snake.BackColor = Color.Green;
+                Snake.Left = ilanParca[ilanParca.Count - 1].Left + 40;
+                Snake.Top = ilanParca[ilanParca.Count - 1].Top;
+                Controls.Add(Snake);
+                ilanParca.Add(Snake);
+                Meal.Hide();
+                CreateMeal();
+            }
+            topPos += 10;
+            Snake.Top = topPos;
+        }
+
+        public void SolaGet()
+        {
+
+            for (int i = 0; i < ilanParca.Count - 1; i++)
+            {
+                ilanParca[i].Top = ilanParca[i + 1].Top;
+                ilanParca[i].Left = ilanParca[i + 1].Left;
+            }
+            int a = 0;
+            foreach (var item in ilanParca)
+            {
+                a++;
+                if (ilanParca.Count == a)
+                {
+                    item.Left += 40;
+                }
+            }
+
+
+
+            if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
+            {
+              
+                
+
+                Snake = new PictureBox();
+                Snake.Width = 10;
+                Snake.Height = 10;
+                Snake.BackColor = Color.Green;
+                Snake.Left = ilanParca[ilanParca.Count - 1].Left + 40;
+                Snake.Top = ilanParca[ilanParca.Count - 1].Top;
+                Controls.Add(Snake);
+                ilanParca.Add(Snake);
+                Meal.Hide();
+                CreateMeal();
+            }
+            leftPos -= 10;
+            Snake.Left = leftPos;
+        }
+
+
+        public void SagaGet()
+        {
+            for (int i = 0; i < ilanParca.Count - 1; i++)
+            {
+                ilanParca[i].Top = ilanParca[i + 1].Top;
+                ilanParca[i].Left = ilanParca[i + 1].Left;
+            }
+            int a = 0;
+            foreach (var item in ilanParca)
+            {
+                a++;
+                if (ilanParca.Count == a)
+                {
+                    item.Left += 40;
+                }
+            }
+
+           
+
+            if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
+            {
+               
+
+
+                Snake = new PictureBox();
+                Snake.Width = 10;
+                Snake.Height = 10;
+                Snake.BackColor = Color.Green;
+                Snake.Left = ilanParca[ilanParca.Count - 1].Left + 40;
+                Snake.Top = ilanParca[ilanParca.Count - 1].Top;
+                Controls.Add(Snake);
+                ilanParca.Add(Snake);
+                Meal.Hide();
+                CreateMeal();
+
+                //ilanParca.Add(Snake);
+                //Snake.Left = ilanParca[ilanParca.Count - 1].Left;
+                //Snake.Top = ilanParca[ilanParca.Count - 1].Top + 40;
+
+            }
+            leftPos += 10;
+            Snake.Left = leftPos;
+        }
+
+        
+
+        private void uP_Click_1(object sender, EventArgs e)
+        {
+           
+            YuxariGet();
+        }
+
+        private void Down_Click_1(object sender, EventArgs e)
+        {
+            
+            AsagiGet();
+        }
+
+        private void Left_Click(object sender, EventArgs e)
+        {
+            
+            SolaGet();
+        }
+
+        private void Right_Click(object sender, EventArgs e)
+        {
+            SagaGet();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
